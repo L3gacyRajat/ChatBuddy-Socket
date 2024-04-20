f6acb60461e55e85276bcc96ef91d2666a3edb0c
Rajat Kumar Singh <rajatkumarsingh2002@gmail.com>
2024-04-20 17:20:39 +0530

    server deployed

--- server/index.js
+++ server/index.js
@@ -22,9 +22,9 @@ app.use(cors());
 const __dirname1 =path.resolve();
 console.log(__dirname1);
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1, '../client/dist')));
+    app.use(express.static(path.join(__dirname1, '/client/dist')));
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1,'../client', 'dist', 'index.html'));
+        res.sendFile(path.resolve(path.join(__dirname1,'/client', 'dist', 'index.html')));
     });
 } else{
     app.use(router);
